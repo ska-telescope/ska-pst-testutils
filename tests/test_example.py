0c77f2e675c3b15491b22b55fe627bfc7b9ef524
jesmigel <jesmigel.developer@gmail.com>
2023-06-28 15:04:21 +1000

    AT3-454 added SKA PST in comments

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,4 +1,10 @@
 # -*- coding: utf-8 -*-
+#
+# This file is part of the SKA PST LMC project
+#
+# Distributed under the terms of the BSD 3-clause new license.
+# See LICENSE for more info.
+
 
 """Tests for the ska_python_skeleton module."""
 import pytest
